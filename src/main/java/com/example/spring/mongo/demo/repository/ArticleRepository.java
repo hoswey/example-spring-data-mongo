b5c3146211fe3f787e2eab6bfeacb1d1099d6a4a
hongshuwei <hongshuwei@gmail.com>
2018-04-21 14:52:14 +0800

    draft

--- src/main/java/com/example/spring/mongo/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/spring/mongo/demo/repository/ArticleRepository.java
@@ -1,12 +1,17 @@
 package com.example.spring.mongo.demo.repository;
 
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
  * @author hoswey
  */
 public interface ArticleRepository extends MongoRepository<Article, String>,
-    ArticleRepositoryCustom {
+    CustomizedArticleRepository {
 
-  Article findByAuthor(String author);
+  Page<Article> findByAuthorOrderByIdDescAllIgnoreCase(String author, Pageable pageable);
+
+  List<Article> findByNumOfLikeIsGreaterThan(int numOfLike);
 }
